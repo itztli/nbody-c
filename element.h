95e58596b842e4cfc47b8d4d61036d68a87ee4e6
Victor De la Luz <itztli@gmail.com>
2020-04-15 12:42:30 -0500

    Element definition

--- element.h
+++ element.h
@@ -5,11 +5,14 @@
 extern "C" {
 #endif /* __cplusplus */
 
-typedef struct{
-  float mass; //kg
-  float position[2]; //[x,y] m
-  float velocity[2]; // [x,y] m/s
-}Element;
+  typedef struct{
+    float mass; //kg
+    float position[2]; //[x,y] m
+    float velocity[2]; // [x,y] m/s
+  }Element;
+
+  Element new_Element(float mass, float p0, float p1, float v0, float v1);
+  int print_Element(Element element);
 
 #ifdef __cplusplus
 } /* extern "C" */
--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
+#include "element.h"
 
 int main(int argn, char **args){
+  Element a1,a2;
 
+  a1 = new_Element(1.0, 0.0,0.0, 1.0,0.0);
+  a2 = new_Element(1.0, 10.0,0.0, -1.0,0.0);
+
+  print_Element(a1);
+  print_Element(a2);
+  
   return 1;
 }
